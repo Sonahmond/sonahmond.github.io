b0716d6ac01913b30c41bece83c9599221ff3b84
Dahlesmass <98613088+Dahlesmass@users.noreply.github.com>
2022-02-22 10:20:31 +0300

    Add files via upload

---
+++ Forum/index.php
@@ -0,0 +1,63 @@
+<!doctype html>
+<html dir="ltr" lang="en">
+<head>
+	<meta name="keywords" content="Sonahmond,Forum">
+	<meta name="description" content="Sonahmond Games - Forum">
+	<meta charset="utf-8">
+
+	<title>Sonahmond - Forum</title>
+
+	<link rel="apple-touch-icon" sizes="57x57" href="Images/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="Images/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="Images/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="Images/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="Images/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="Images/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="Images/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="Images/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="Images/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192"  href="Images/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="Images/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="Images/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="Images/favicon-16x16.png">
+    <link rel="manifest" href="Images/manifest.json">
+    <link rel="stylesheet" type="text/css" href="Style/index.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@300&display=swap" rel="stylesheet">
+    <meta name="msapplication-TileColor" content="#fff">
+    <meta name="msapplication-TileImage" content="Images/ms-icon-144x144.png">
+    <meta name="theme-color" content="#ffffff">
+</head>
+<body>
+	<header id="header" class="header">
+		<img id="logo" class="logo" src="Images/apple-icon-180x180.png">
+		<br>
+		<nav id="nav" class="nav">
+			<a id="home" class="home" href="https://sonahmond.github.io">Home</a>
+			<br>
+			<a id="help" class="help" href="https://sonahmond.github.io/Help/">Help</a>
+			<br>
+			<a id="serch" class="serch" href="https://sonahmond.github.io/Serch/">Serch</a>
+			<br>
+			<a id="forum" class="forum" href="https://sonahmond.github.io/Forum/">Forum</a>
+		</nav>
+		<br>
+		<button id="sign-up" class="sign-up" onclick="window.location.href = 'https://sonahmond.github.io/SignUp/';"><b>Sign Up</b></button>
+	</header>
+	<br>
+	<main id="main" class="main">
+		<p id="text" class="text">Forum is closed!</p>
+	</main>
+	<div id="basement" class="basement">
+		<h1 id="heading-1" class="heading-1">Website</h1>
+		<a id="source" class="source" href="https://github.com/Sonahmond/sonahmond.github.io"><b>Source</b></a>
+		<a id="repository" class="repository" href="https://github.com/Sonahmond/sonahmond.github.io"><b>Repository</b></a>
+		<h1 id="heading-2" class="heading-2">Contact Us</h1>
+		<p id="email" class="email"><b>E-mail: sanahmond@gmail.com</b></p>
+		<a id="website" class="website" href="https://sonahmond.github.io"><b>Website: sonahmond.github.io</b></a>
+		<h1 id="heading-3" class="heading-3">Social networks</h1>
+		<a id="telegram" class="telegram" href="https://t.me/sonahmond"><b>Telegram: t.me/sonahmond</b></a>
+	</div>
+</body>
+</html>
